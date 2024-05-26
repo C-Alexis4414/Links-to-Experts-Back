25f654867ea7a11ecf4a8a670b24ee9f477ad90b
accardigianni <gianni.accardi@epitech.eu>
2024-05-26 19:37:35 +0200

    create function ok

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,11 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
     private prisma = new PrismaClient();
 
-    async getUsers() {
+    async getUsers(): Promise<User[]> {
         return this.prisma.user.findMany();
     }
+
+    async createUser(
+        First_name: string,
+        Last_Name: string,
+        Password: string,
+        Email_address: string,
+        Is_Youtuber: boolean,
+        Is_Pro: boolean
+    ): Promise<User> {
+        return this.prisma.user.create({
+            data: {
+                First_name,
+                Last_Name,
+                Password,
+                Email_address,
+                Is_Youtuber,
+                Is_Pro
+            },
+        });
+    }
+
+    async updateUser(
+        id: number,
+        First_name?: string,
+        Last_Name?: string,
+        Password?: string,
+        Email_address?: string,
+        Is_Youtuber?: boolean,
+        Is_Pro?: boolean
+    ): Promise<User> {
+        return this.prisma.user.update({
+            where: { id },
+            data: {
+                First_name,
+                Last_Name,
+                Password,
+                Email_address,
+                Is_Youtuber,
+                Is_Pro
+            },
+        });
+    }
+
+    async deleteUser(id: number): Promise<User> {
+        return this.prisma.user.delete({
+            where: { id },
+        });
+    }
 }
+
