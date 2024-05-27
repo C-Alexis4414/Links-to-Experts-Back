a946adedc8557ba79076bbee5510308300be8a7d
accardigianni <gianni.accardi@epitech.eu>
2024-05-27 22:13:12 +0200

    CRUD: get create delete and update run with route

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -6,12 +6,17 @@ import { User } from '@prisma/client';
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
-    @Get()
+    @Get('getUsers')
     async getUsers(): Promise<User[]> {
         return this.userService.getUsers();
     }
 
-    @Post('chocolate')
+    @Get(':id')
+    async getUser(@Param('id') id: number): Promise<User> {
+        return this.userService.getUser(id);
+    }
+
+    @Post('create')
     async createUser(): Promise<User> {
         const data = {
             "First_name": "gianni",
@@ -24,32 +29,35 @@ export class UserController {
         const newUser = await this.userService.createUser(data.First_name, data.Last_Name, data.Password, data.Email_address, data.Is_Youtuber, data.Is_Pro);
         return newUser
     }
+
+
+    @Put('update')
+    async updateUser(
+        // @Param('id') id: number,
+        // @Param('First_name') First_name?: string,
+        // @Param('Last_Name') Last_Name?: string,
+        // @Param('Password') Password?: string,
+        // @Param('Email_address') Email_address?: string,
+        // @Param('Is_Youtuber') Is_Youtuber?: boolean,
+        // @Param('Is_Pro') Is_Pro?: boolean
+    ): Promise<User> {
+        const id = 7
+        const First_name = "henry";
+        const updateUser = await this.userService.updateUser(
+            id,
+            First_name,
+            // Last_Name,
+            // Password,
+            // Email_address,
+            // Is_Youtuber,
+            // Is_Pro
+        );
+        return updateUser;
+    }
+
+    @Delete('delete/:id')
+    async deleteUser(@Param('id') id: number): Promise<User> {
+        return this.userService.deleteUser(id);
+    }
 }
-
-// @Put(':id')
-// async updateUser(
-//     @Param('id') id: number,
-//     @Body('First_name') First_name ?: string,
-//     @Body('Last_Name') Last_Name ?: string,
-//     @Body('Password') Password ?: string,
-//     @Body('Email_address') Email_address ?: string,
-//     @Body('Is_Youtuber') Is_Youtuber ?: boolean,
-//     @Body('Is_Pro') Is_Pro ?: boolean
-// ): Promise < User > {
-//     return this.userService.updateUser(
-//         id,
-//         First_name,
-//         Last_Name,
-//         Password,
-//         Email_address,
-//         Is_Youtuber,
-//         Is_Pro
-//     );
-// }
-
-// @Delete(':id')
-// async deleteUser(@Param('id') id: number): Promise < User > {
-//     return this.userService.deleteUser(id);
-// }
-// }
 
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -9,6 +9,10 @@ export class UserService {
         return this.prisma.user.findMany();
     }
 
+    async getUser(id: number): Promise<User> {
+        return this.prisma.user.findUnique({ where: { id: Number(id) } });
+    }
+
     async createUser(
         First_name: string,
         Last_Name: string,
@@ -17,16 +21,17 @@ export class UserService {
         Is_Youtuber: boolean,
         Is_Pro: boolean
     ): Promise<User> {
-        return this.prisma.user.create({
+        const newUser = await this.prisma.user.create({
             data: {
-                First_name,
-                Last_Name,
-                Password,
-                Email_address,
-                Is_Youtuber,
-                Is_Pro
+                First_name: First_name,
+                Last_Name: Last_Name,
+                Password: Password,
+                Email_address: Email_address,
+                Is_Youtuber: Is_Youtuber,
+                Is_Pro: Is_Pro
             },
         });
+        return newUser;
     }
 
     async updateUser(
@@ -38,22 +43,32 @@ export class UserService {
         Is_Youtuber?: boolean,
         Is_Pro?: boolean
     ): Promise<User> {
-        return this.prisma.user.update({
-            where: { id },
+        const user = await this.getUser(id);
+        console.log(First_name);
+
+
+        const updateUser = await this.prisma.user.update({
+            where: { id: Number(id) },
             data: {
-                First_name,
-                Last_Name,
-                Password,
-                Email_address,
-                Is_Youtuber,
-                Is_Pro
+                First_name: First_name,
+                Last_Name: user.Last_Name,
+                Password: user.Password,
+                Email_address: user.Email_address,
+                Is_Youtuber: user.Is_Youtuber,
+                Is_Pro: user.Is_Pro,
+
             },
         });
+        console.log(First_name);
+
+        console.log(updateUser);
+
+        return updateUser;
     }
 
     async deleteUser(id: number): Promise<User> {
         return this.prisma.user.delete({
-            where: { id },
+            where: { id: Number(id) },
         });
     }
 }
