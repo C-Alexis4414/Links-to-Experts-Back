aeb75a0c0af61d997163b9eee63ffff32fc832c1
accardigianni <gianni.accardi@epitech.eu>
2024-05-31 16:36:45 +0200

    create jwt token ok, find how to use it in auth service

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -16,9 +16,15 @@ export class UserService {
         return await this.prisma.user.findUnique({ where: { id: Number(id) } });
     }
 
-    async signIn(userName: string, password: string): Promise<UserType[]> {
-        return await this.prisma.user.findMany({ where: { First_name: userName, Password: password } });
+    async signIn(userName: string, password: string): Promise<UserType> {
+        const user = await this.prisma.user.findFirst({ where: { First_name: userName, Password: password } });
+        return user
     }
+    async findName(userName: string): Promise<UserType> {
+        const user = await this.prisma.user.findFirst({ where: { First_name: userName } });
+        return user
+    }
+
 
     async createUser(createUserDto: CreateUserDto): Promise<UserType> {
         const newUser = await this.prisma.user.create({
