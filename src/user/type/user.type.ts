f7aefc903d5d86618b18bc97ce0f5559d820bbec
accardigianni <gianni.accardi@epitech.eu>
2024-07-07 00:42:56 +0200

    User CRUD first step

--- src/user/type/user.type.ts
+++ src/user/type/user.type.ts
@@ -1,10 +1,8 @@
 
 export type UserType = {
-    id: number;
-    First_name: string;
-    Last_Name: string;
-    Password: string;
-    Email_address: string;
-    Is_Youtuber: boolean;
-    Is_Pro: boolean;
+    userName: string;
+    password: string;
+    email: string;
+    is_Youtuber: boolean;
+    is_Professional: boolean;
 }
