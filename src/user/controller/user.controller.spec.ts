5d93ccfbc491f69dac2c56a3f4dfc06da4aced58
accardigianni <gianni.accardi@epitech.eu>
2024-07-21 23:57:21 +0200

    fixbug: run server ok

---
+++ src/user/controller/user.controller.spec.ts
@@ -0,0 +1,22 @@
+// import { UserService } from "../service/user.service";
+// import { ProfessionalService } from "../service/professional.service";
+// import { YoutuberService } from "../service/youtuber.service";
+// import { UserController } from "./user.controller";
+// import { UserType } from "../type/user.type";
+
+// describe("UserController", () => {
+//     let userController: UserController;
+//     let userService: UserService;
+//     let professionalService: ProfessionalService;
+//     let youtuberService: YoutuberService;
+//     beforeEach(() => {
+//         userController = new UserController(userService, youtuberService, professionalService);
+//     });
+
+//     describe('findOneUserById', () => {
+//         it('should return information about the user with typed data', async () => {
+
+//         })
+//     })
+// })
+
