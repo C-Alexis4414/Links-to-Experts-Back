007bd5ba0a47ec56e95e8eb8a992de48856dce59
accardigianni <gianni.accardi@epitech.eu>
2024-07-21 23:56:30 +0200

    server running + set model file organisation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
-import { AuthModule } from './authentification/auth.module';
+import { LikesModule } from './liked/liked.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './authentification/auth.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule],
+  imports: [UserModule, CategoryModule, LikesModule],
   controllers: [],
   providers: [],
 
