c347543560b419b9b2f10e0f81aef19f7691bae6
C-Alexis4414 <a.chentre@protonmail.com>
2025-02-17 21:38:20 +0100

    feat: YL-19 LikedTag table added
    - LikedTag table created in schema.prisma (changes pushed to DB)
    - LikedTag service, controller and module created
    - In userData.dto and user.service error likendin corrected in linkedin
    
    Reviewed by: @C-Alexis4414
    Jira issue: YL-19 LikedTagTable

---
+++ src/likedTag/likedTag.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { LikedTagController } from './controller/likedTag.controller';
+import { LikedTagService } from './service/likedTag.service';
+import { PrismaService } from 'src/prisma.service';
+import { UserModule } from 'src/user/user.module';
+import { TagsModule } from 'src/tags/tag.module';
+
+@Module({
+    providers: [PrismaService, LikedTagService],
+    controllers: [LikedTagController],
+    exports: [],
+    imports: []
+})
+export class LikedTagModule {
+}
