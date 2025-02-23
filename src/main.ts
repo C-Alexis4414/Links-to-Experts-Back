89b177b9054a2fe2b586e92491bb16280c1f3ae4
C-Alexis4414 <a.chentre@protonmail.com>
2025-02-23 21:01:36 +0100

    build: YL-27 Communication ok with front not dockerized + DB migrated in the pgadmin container

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
+  app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Swagger Links-to-experts')
     .setDescription('testing API address')
@@ -29,8 +34,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173', 
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(3000, '0.0.0.0');
