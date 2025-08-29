1ba86bc87d360af3fe522fb7c2ff86d05d7532ba
C-Alexis4414 <a.chentre@protonmail.com>
2025-08-29 20:51:31 +0200

    chore(deps): Migration .eslintrc.js to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    resolvePluginsRelativeTo: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    // mimic ESLintRC-style extends
+    ...compat.extends('.eslintrc.js'),
+];
